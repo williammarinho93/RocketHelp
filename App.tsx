7e3b4d79080d6e133e26ba6ae8acc8b1d3df84af
williammarinho93 <willmarinhosantos93@gmail.com>
2022-07-20 17:52:24 -0300

    instalando o reactbase

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
+import { NativeBaseProvider } from 'native-base';
+
+import { THEME } from './src/styles/theme';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <SignIn />
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
