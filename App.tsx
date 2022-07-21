2d6f4d64ea556c11f25d8d53f3c4f7ae839b8d13
williammarinho93 <willmarinhosantos93@gmail.com>
2022-07-21 12:13:11 -0300

    criando forma na tela de signin

--- App.tsx
+++ App.tsx
@@ -1,14 +1,26 @@
 import { SignIn } from './src/screens/SignIn';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
 
 import { THEME } from './src/styles/theme';
 
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold }); //carregamento de fonte
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+
+      />
+      { fontsLoaded ? <SignIn /> : <Loading/> }     
     </NativeBaseProvider>
-  );
+  ); //fontes carregaram? o fontsLoaded é verdadeiro(?) se for vedadeiro carrega a tela de login caso contrario carega a tela de loading
 }
 
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,17 @@
 
-import { View, Text } from 'react-native';
+import { VStack, Heading } from 'native-base';
+
+import Logo from '../assets/logo_primary.svg'; //exportando a logo e criando o componente <Logo/>
 
 export function SignIn(){
     return(
-        <View>
-            <Text>William</Text>
-        </View>
+        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+            <Logo /> 
+
+
+            <Heading color="gray.100">
+                Acesse sua conta
+            </Heading>
+        </VStack>
     )
 }
